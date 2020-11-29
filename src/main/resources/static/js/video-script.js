26ad70da471aab66070021e5c9908859e67a61cf
Fabricio Vasconcellos <fabricio.vasconc@gmail.com>
2020-11-29 18:51:14 -0300

    Improvements

--- src/main/resources/static/js/video-script.js
+++ src/main/resources/static/js/video-script.js
@@ -83,7 +83,7 @@ video.addEventListener('play', async () => {
 })
 
 function loadLabeledImages() {
-  const labels = ['Tony Stark', 'Black Widow', 'Captain America', 'Captain Marvel', 'Thor']
+  const labels = ['Fabrício Vasconcellos', 'Rafael Ribeiro']
   return Promise.all(
     labels.map(async label => {
       const descriptions = [];
@@ -96,3 +96,39 @@ function loadLabeledImages() {
     })
   )
 }
+
+// const vid = document.querySelector('video');
+// navigator.mediaDevices.getUserMedia({video: true}) // request cam
+//     .then(stream => {
+//       vid.srcObject = stream; // don't use createObjectURL(MediaStream)
+//       return vid.play(); // returns a Promise
+//     })
+//     .then(()=>{ // enable the button
+//       const btn = document.querySelector('button');
+//       btn.disabled = false;
+//       btn.onclick = e => {
+//         takeASnap()
+//             .then(download);
+//       };
+//     })
+//     .catch(e=>console.log('please use the fiddle instead'));
+//
+// function takeASnap(){
+//   const canvas = document.createElement('canvas'); // create a canvas
+//   const ctx = canvas.getContext('2d'); // get its context
+//   canvas.width = vid.videoWidth; // set its size to the one of the video
+//   canvas.height = vid.videoHeight;
+//   ctx.drawImage(vid, 0,0); // the video
+//   return new Promise((res, rej)=>{
+//     canvas.toBlob(res, 'image/jpeg'); // request a Blob from the canvas
+//   });
+// }
+// async function download(blob){
+//   // uses the <a download> to download a Blob
+//   let a = document.createElement('a');
+//   a.href = URL.createObjectURL(blob);
+//   a.download = 'screenshot.jpg';
+//   document.body.appendChild(a);
+//   await a.click();
+//   await loadLabeledImages();
+// }
